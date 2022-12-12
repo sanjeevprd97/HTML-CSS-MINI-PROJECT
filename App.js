24ca5d115ae372b9e26a814e383f5784af88e226
sanjeevprd97 <81824723+sanjeevprd97@users.noreply.github.com>
2022-12-12 23:17:49 +0530

    mini project

--- App.js
+++ App.js
@@ -1,1 +1,21 @@
 
+const contents = document.querySelectorAll('.image')
+const listItem = document.querySelectorAll('nav ul li')
+
+listItem.forEach((item, idx) => {
+    item.addEventListener('click', () => {
+        hideAllContents()
+        hideAllItems()
+
+        item.classList.add('active')
+        contents[idx].classList.add('show')
+    })
+})
+
+function hideAllContents() {
+    contents.forEach(content => content.classList.remove('show'))
+}
+
+function hideAllItems() {
+    listItem.forEach(item => item.classList.remove('active'))
+}
